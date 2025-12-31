c96d7d6f72716a9e0a4f251731c46fc79d191024
fgnzalez-sudo <fgnzalez@gmail.com>
2025-12-30 22:07:00 -0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>El Shaddai Revival Church</h1>
+      <p>Bienvenidos a nuestra iglesia.</p>
+
+      <p>
+        <strong>Servicios:</strong><br />
+        Miércoles 7:30 PM<br />
+        Domingo 10:00 AM
+      </p>
+
+      <p>
+        <a href="https://www.elshaddairevivalchurch.com/" target="_blank">
+          Visitar sitio web
+        </a>
+      </p>
+    </div>
+  );
+}
