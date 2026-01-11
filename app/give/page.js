0d268dc4555af0121d10bca1704cc01319440100
fgnzalez-sudo <fgnzalez@gmail.com>
2026-01-10 20:15:02 -0600

    Create page.js

---
+++ app/give/page.js
@@ -0,0 +1,11 @@
+export default function GivePage() {
+  return (
+    <div className="stack">
+      <section className="card">
+        <h1>Give / Donar</h1>
+        <p>You can support the church via Zelle:</p>
+        <p style={{ fontSize: 20, fontWeight: 800 }}>(832) 693-5950</p>
+      </section>
+    </div>
+  );
+}
