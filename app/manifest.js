49997ff7eac56eeab0945632673d475faf178150
fgnzalez-sudo <fgnzalez@gmail.com>
2026-01-10 20:12:53 -0600

    Create manifest.js

---
+++ app/manifest.js
@@ -0,0 +1,15 @@
+export default function manifest() {
+  return {
+    name: "El Shaddai Revival Church",
+    short_name: "El Shaddai",
+    description: "Church app (PWA) | Houston, TX",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#0b1220",
+    theme_color: "#0b1220",
+    icons: [
+      { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { src: "/icon-512.png", sizes: "512x512", type: "image/png" }
+    ]
+  };
+}
